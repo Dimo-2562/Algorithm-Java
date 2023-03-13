8a700915bba9a3eec43fc54938da7f7016e4fbf5
dmori <chosehoon2@naver.com>
2023-03-13 16:11:13 +0900

    Solve Baekjoon 2309
    Difficulty: Bronze 1
    Algorithm: Brute force, sorting

--- src/src/main/java/org/example/Main.java
+++ src/src/main/java/org/example/Main.java
@@ -1,24 +1,40 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int num = 0;
-        int max = -999;
+        int[] hobit = new int[9];
+        int sum = 0;
+        boolean flag = false;
 
-        for (int i = 0; i < 10; i++) {
-            int minus = sc.nextInt();
-            int plus = sc.nextInt();
-
-            num += (plus - minus);
-            if (max < num)
-                max = num;
-
+        for (int i = 0; i < 9; i++) {
+            hobit[i] = sc.nextInt();
+        }
+        for (int i = 0; i < hobit.length; i++) {
+            sum += hobit[i];
         }
 
-        System.out.println(max);
+        for (int i = 0; i < 9; i++) {
+            if (flag == true)
+                break;
+            for (int j = i + 1; j < 9; j++) {
+                if ((sum - hobit[i] - hobit[j]) == 100) {
+                    hobit[i] = 999;
+                    hobit[j] = 999;
+                    flag = true;
+                    break;
+                }
+            }
+        }
+
+        Arrays.sort(hobit);
+        for (int i = 0; i < 7; i++) {
+            System.out.println(hobit[i]);
+        }
+
     }
 }
