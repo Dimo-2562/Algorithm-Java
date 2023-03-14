630bfb812a2b554c67f667b786f7f90b0e662163
dmori <chosehoon2@naver.com>
2023-03-14 22:42:44 +0900

    Solve Baekjoon 1292
    Difficulty: Bronze 1
    Algorithm: Implementation

--- src/src/main/java/org/example/Main.java
+++ src/src/main/java/org/example/Main.java
@@ -1,24 +1,33 @@
 package org.example;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int t;
-        t = sc.nextInt();
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
 
-        for (int i = 0; i < t; i++) {
-            int[] num = new int[10];
-            for (int j = 0; j < 10; j++) {
-                num[j] = sc.nextInt();
+        int[] num = new int[1002];
+        int count = 1;
+
+        for (int i = 1; i <= 1000; i++) {
+            for (int j = 0; j < i; j++) {
+                if (count == 1001)
+                    break;
+                num[count++] = i;
             }
-
-            Arrays.sort(num);
-
-            System.out.println(num[7]);
+            if (count == 1001)
+                break;
         }
+
+        int sum = 0;
+        for (int i = a; i <= b; i++) {
+            sum += num[i];
+        }
+
+        System.out.println(sum);
     }
 }
